d0aae884eba1d925431d313279be0ca140ef6282
Nigel Babu <nigelbabu@gmail.com>
2021-10-14 21:12:54 +0100

    Make the converter super generic

--- cmpu1026/general_converter.py
+++ cmpu1026/general_converter.py
@@ -6,101 +6,22 @@ This program will ask the user to input a decimal number and this program will
 convert it into a binary and print it out and convert the binary back to
 decimal and print out. It will do the same thing for hexadecimal as well.
 """
-def bin_convert(number):
-    """Perform binary conversion.
-
-    Convert to binary and back to decimal from the binary string. Prints out both values
-
-    Args:
-        number: A decimal number.
-
-    Returns:
-	None. The outputs are printed to stdout.
-
-    """
-    if number < 0:
-        print(f"{number} is negative")
-        return
-    if number == 0:
-        print(f"Binary: {number}")
-        print(f"Decimal: {number}")
-        return
-
-    # create a list to hold the individual digits of binary number
-    binstore = []
-    while number > 0:
-        # Keep appending the remainders to the list
-        binstore.append(str(number % 2))
-        # Reduce the number down to smaller sizes
-        number = number // 2
-    # The reminders need to be reversed
-    binstore.reverse()
-    # Join the items of the list into one string
-    binary = ''.join(binstore)
-    print('Binary:', binary)
-
-    decimal = 0
-    pos = 0
-    while binary:
-        # Extract the last digit
-        digit = int(binary[-1])
-        # Pop the last digit from the binary
-        binary = binary[:-1]
-        # Convert each digit to decimal value
-        decimal += (2 ** pos) * digit
-        pos += 1
-    print('Decimal:', decimal)
 
 
-def hex_convert(number):
-    """Perform hexadecimal conversion.
+def to_decimal(number, base):
+    """Convert a number to decimal from another base
 
-    Convert to hexadecimal and back to decimal from the hexadecimal string. Prints out both values
+    Convert a number from any base into decimal.
 
     Args:
-        number: A decimal number.
+        number: A string representing a number in base 2 to 16.
+        base: The base of the number.
 
     Returns:
-	None. The outputs are printed to stdout.
+        An integer in base 10 of the number provided
 
     """
-    if number < 0:
-        print(f"{number} is negative")
-        return
-    if number <10:
-        print(f"Hexadecimal: {number}")
-        print(f"Decimal: {number}")
-        return
-    # Hold a conversion dict for ease of lookups
-    int_to_hex = {
-        10: 'A',
-        11: 'B',
-        12: 'C',
-        13: 'D',
-        14: 'E',
-        15: 'F',
-    }
-    # A list to hold each character before conversion
-    hexstore = []
-    while number > 0:
-        # The reminder is the digit
-        digit = number % 16
-        number = number // 16
-        # If the number does not have to be converted
-        if digit < 10:
-            hexstore.append(str(digit))
-        else:
-            # Lookup the corresponding alphabet
-            hexstore.append(int_to_hex[digit])
-    # Reverse the list before joining and printing
-    hexstore.reverse()
-    hexadecimal = ''.join(hexstore)
-    print('Hexadecimal:', hexadecimal)
-
-    decimal = 0
-    pos = 0
-    # Conversion dict that's reverse the previous one
-    hex_to_int = {
+    converter = {
         'A': 10,
         'B': 11,
         'C': 12,
@@ -108,19 +29,59 @@ def hex_convert(number):
         'E': 14,
         'F': 15,
     }
-    while hexadecimal:
+    decimal = 0
+    pos = 0
+    while number:
         # Extract digits from the back of the number
-        digit = hexadecimal[-1]
-        hexadecimal = hexadecimal[:-1]
+        digit = number[-1]
+        number = number[:-1]
         # Get the actual number in case it's an alphabet
-        if hex_to_int.get(digit, None):
-            digit = hex_to_int[digit]
+        if converter.get(digit, None):
+            digit = converter[digit]
         else:
             digit = int(digit)
         # Perform the multiplications
-        decimal += (16 ** pos) * digit
+        decimal += (base ** pos) * digit
         pos += 1
-    print('Decimal:', decimal)
+    return decimal
+
+
+def to_base(number, base):
+    """Convert a decimal to another other base.
+
+    Convert a base 10 number to any base from 2 to 16.
+
+    Args:
+        number: A decimal number.
+        base: The base of the provided number.
+
+    Returns:
+        A string with the number in a requested base.
+
+    """
+    # Hold a conversion dict for ease of lookups
+    converter = {
+            10: 'A',
+            11: 'B',
+            12: 'C',
+            13: 'D',
+            14: 'E',
+            15: 'F',
+    }
+    # A list to hold each character before conversion
+    store = []
+    while number > 0:
+        # The reminder is the digit
+        digit = number % base
+        number = number // base
+        if converter.get(digit, None):
+            # Lookup the corresponding alphabet
+            store.append(converter[digit])
+        else:
+            store.append(str(digit))
+    # Reverse the list before joining and printing
+    store.reverse()
+    return ''.join(store)
 
 
 def main():
@@ -129,12 +90,30 @@ def main():
     Requests input and calls the bin_convert and hex_convert functions.
 
     Returns:
-	None. The outputs are printed to stdout.
+        None. The outputs are printed to stdout.
 
     """
-    num = input("Provide a decimal numer ")
-    bin_convert(int(num))
-    hex_convert(int(num))
+    num = input("Provide a number ")
+    if not num.isalnum():
+        print(f"{num} is not alphanumeric")
+        return
+    src_base = input("What is the base for this number? ")
+    if not src_base.isnumeric():
+        print(f"{src_base} is not a number")
+        return
+    dest_base = input("What base would you like to convert to (2-16)? ")
+    if not dest_base.isnumeric():
+        print(f"{dest_base} is not a number")
+        return
+    if int(src_base) == int(dest_base):
+        print("The source and destination are the same")
+    if src_base != 10:
+        number = to_decimal(num.strip(), int(src_base))
+    if int(dest_base) == 10:
+        converted = number
+    else:
+        converted = to_base(number, int(dest_base))
+    print(f"{num} in base {src_base} is {converted} in base {dest_base}")
 
 
 if __name__ == '__main__':
