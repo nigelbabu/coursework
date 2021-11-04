aade1da7ade06ba68f2fd773a48a4974c03e0696
Nigel Babu <nigelbabu@gmail.com>
2021-11-04 20:58:53 +0000

    Simplify the code a bit and add music

--- cmpu1026/music_reader.py
+++ cmpu1026/music_reader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Print index, title, time signature, and key signature from an abc file
 
 Sample output of the following form:
@@ -8,6 +8,20 @@ Sample output of the following form:
     198 ... Knotted Cord, The ... Time sig: C| ... Key sig: Ador ...
 
 """
+from pygame import mixer
+import time
+
+
+
+def play_music(file_name='/home/nigel/coursework/cmpu1026/killavil_fancy.mp3'):
+    """Play music for 30 seconds
+    """
+    mixer.init()
+    mixer.music.load(file_name)
+    mixer.music.set_volume(0.7)
+    mixer.music.play()
+    time.sleep(10)
+    mixer.music.fadeout(500)
 
 
 def clean(line):
@@ -32,13 +46,13 @@ def main():
             if line.startswith('X:'):
                 song_data['index'] = clean(line)
             # Find the title, only use the first item that starts with T:
-            if line.startswith('T:'):
-                if song_data.get('title') is None:
-                    song_data['title'] = clean(line)
+            if line.startswith('T:') and song_data.get('title') is None:
+                song_data['title'] = clean(line)
             # Find the time signature
             if line.startswith('M:'):
                 song_data['time_sig'] = clean(line)
-            # Find the key signature
+            # Find the key signature and print everything because this should be
+            # a complete record
             if line.startswith('K:'):
                 song_data['key_sig'] = clean(line)
                 print(f"{song_data['index']} ... {song_data['title']}",
@@ -46,6 +60,7 @@ def main():
                       f"Key sig: {song_data['key_sig']} ... ")
                 # Reset the song data to an empty dict
                 song_data = {}
+    play_music()
 
 
 
